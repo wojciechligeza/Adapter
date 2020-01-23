b844bcb670936c13f639f28f321502307364e85e
Wojciech <wojciech.ligeza@gmail.com>
2020-01-24 00:43:03 +0100

    initial

---
+++ Adapter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adapter
+{
+    class ThirdPartyEmployee
+    {
+        public List<string> GetEmployeeList()
+        {
+            List<string> EmployeeList = new List<string>();
+
+            EmployeeList.Add("Piotr");
+            EmployeeList.Add("Zbyszek");
+            EmployeeList.Add("Heniu");
+            EmployeeList.Add("Roman");
+
+            return EmployeeList;
+        }
+    }
+
+    interface ITarget
+    {
+        List<string> GetEmployees();
+    }
+
+    class EmployeeAdapter : ThirdPartyEmployee, ITarget
+    {
+        public List<string> GetEmployees()
+        {
+            return GetEmployeeList();
+        }
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista pracowników od zewnetrznej organizacji");
+            Console.WriteLine("---------------------------------------------");
+
+            ITarget adapter = new EmployeeAdapter();
+
+            foreach (string employee in adapter.GetEmployees())
+            {
+                Console.WriteLine(employee);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# WZORZEC ADAPTER
+
+Aplikacja konsolowa pokazująca zastosowanie wzorca adaptera na przykładzie
+pobierania listy pracowników z zewnętrznej firmy.
+
+# RUN
+
+z konsoli, bez podawania parametru
